f7e266c80eaecdfd3a9269f81f61ebd7adce74e7
Zafar-Ali1 <xafar5551@gmail.com>
2023-02-05 13:52:49 -0800

    html file added

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+ 
+//use the application off of express.
+var app = express();
+var port=8080;
+
+//define the route for "/"
+app.get("/", function (request, response){
+    //show this file when the "/" is requested
+    response.sendFile(__dirname+"/index.html");
+});
+
+//start the server
+app.listen(8080);
+
+console.log("Something awesome to happen at http://localhost:8080");
